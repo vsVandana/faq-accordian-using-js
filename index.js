72874819da7744a2a192dea1c6ecf3779a8a21fd
vsvandana <vandananawhall@gmail.com>
2024-02-02 20:23:52 +0530

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,9 @@
+var expand_icons = document.getElementsByClassName("plus-icon")
+
+
+let AllAnswers = document.getElementsByTagName("p")
+
+for(let i =0; i < AllAnswers.length ; i++){
+    console.log(`${AllAnswers[i].textContent}`);
+}
+
